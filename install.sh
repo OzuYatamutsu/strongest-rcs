5fe2a06a64c941d403ef24a0747cf3cfa89143bf
Sean Collins <seanc@spiceworks.com>
2016-03-10 13:24:04 -0600

    Detect fish location for tmux.conf

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get location of fish
+sed "s|default-shell[[:space:]].*$|default-shell $(which fish)|g" tmux.conf > tmux_temp
+mv -fv tmux_temp ~/.tmux.conf
+
 cp -fv config.fish ~/.config/fish/
 cp -fv vimrc ~/.vimrc
-cp -fv tmux.conf ~/.tmux.conf
