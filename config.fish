876a8405e44b276e84308fd95ae118a18b4df947
Sean Collins <seanc@spiceworks.com>
2016-09-29 09:43:39 -0500

    !!, !$ support

--- config.fish
+++ config.fish
@@ -176,6 +176,33 @@ function ____fish_git_remote_info
 end
 ### END GIT STUFF
 
+### START !!, !$ bash support
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+function fish_user_key_bindings
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
+## END !!, !$ bash support
+
+
 function user_hostname_prompt --description "Displays the username and the hostname"
     echo -n "$USER@"
     echo -n (hostname)
