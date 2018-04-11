3590af734b81822bdcfc96e726b100cc2357d481
Sean Collins <secollins@linkedin.com>
2018-04-11 11:26:56 -0700

    compatible ascii stuff, sad :c

--- plugins/daysuntil.py
+++ plugins/daysuntil.py
@@ -8,7 +8,8 @@ days_until = str((TARGET_DATE - date.today()).days)
 
 # Return to evaling shell
 print(
-    "printf ' ✨There are '; {days} printf 'until '; {target_date_humanized} printf '.\n'".format(
+    "{icon} printf ' There are '; {days} printf 'until '; {target_date_humanized} printf '.\n'".format(
+        icon=emphasize_text('blue', ' i'),
         days=emphasize_text('blue', '{days_until} days '.format(days_until=days_until)),
         target_date_humanized=emphasize_text('green', TARGET_DATE.strftime('%B %d, %Y'))
     )
