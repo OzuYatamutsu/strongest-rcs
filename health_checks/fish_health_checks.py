4391832f5580ab939d2423a0c934e7270c7e7933
Sean Collins <seanc@seancotech.com>
2018-05-14 09:31:57 -0700

    Use endpoint constant

--- health_checks/fish_health_checks.py
+++ health_checks/fish_health_checks.py
@@ -12,9 +12,9 @@ class FishHealthChecks(HealthCheckBase):
         """Checks network connectivity."""
 
         net_command = (
-            "nc -zw1 google.com 80 2>/dev/null" if not isfile("/etc/redhat-release")
-            else "nc -w1 google.com 80 --send-only </dev/null 2>/dev/null"
-        )
+            "nc -zw1 {endpoint} 80 2>/dev/null" if not isfile("/etc/redhat-release")
+            else "nc -w1 {endpoint} 80 --send-only </dev/null 2>/dev/null"
+        ).format(endpoint=FishHealthChecks._net_check_endpoint)
 
         try:
             result = check_output(
