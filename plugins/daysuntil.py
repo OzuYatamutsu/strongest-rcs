cb0e18d0d3df61b22697e9f4c9b43eaa019ac189
Sean Collins <secollins@linkedin.com>
2018-06-29 12:43:05 -0700

    Update next date

--- plugins/daysuntil.py
+++ plugins/daysuntil.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from datetime import date
 
-TARGET_DATE = date(2018, 6, 30)
+TARGET_DATE = date(2018, 8, 27)
 days_until = (TARGET_DATE - date.today()).days
 
 result = " <blue>i<reset> There {are_or_is} <blue>{days} {days_or_day}<reset> until <green>{target_date_humanized}.".format(
