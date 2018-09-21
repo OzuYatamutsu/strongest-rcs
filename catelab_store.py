f8b42226233ae821fa55716adc6eb8349c8fb4b3
Sean Collins <seanc@seancotech.com>
2018-09-21 10:44:22 +0100

    Add SQLite store and unit test

---
+++ catelab_store.py
@@ -0,0 +1,51 @@
+from sqlite3 import connect, Row
+from os.path import join
+
+
+class CatelabStore:
+    _METADATA_STORE_FILENAME = 'catelab_metadata.sqlite3'
+    __KEY_TABLE = 'metadata'
+
+    def __init__(self, base_config_dir: str):
+        self.conn = connect(join(base_config_dir, CatelabStore._METADATA_STORE_FILENAME))
+        self.conn.row_factory = Row
+        self.conn.execute("CREATE TABLE IF NOT EXISTS metadata(key TEXT PRIMARY KEY, value TEXT)")
+        self.conn.commit()
+
+    def get_last_update_utime(self) -> int:
+        return load_config_key('LAST_UPDATE_UTIME')
+
+    def get_catelab_source_dir(self) -> str:
+        return load_config_key('SOURCE_DIR')
+
+    def write_config_key(self, key: str, value: str) -> str:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            f"INSERT OR REPLACE INTO {CatelabStore.__KEY_TABLE}(key, value) "
+            "VALUES (?, ?)", (key, value)
+        )
+
+        self.conn.commit()
+
+    def load_config_key(self, key: str) -> str:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            f"SELECT value FROM {CatelabStore.__KEY_TABLE} "
+            "WHERE key = ?", (key,)
+        )
+
+        result = cursor.fetchone() or {}
+        result = dict(result)
+
+        if not result:
+            return ""
+        return result['value']
+
+    def clear(self) -> None:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            f"DELETE FROM {CatelabStore.__KEY_TABLE}"
+        )
+
+        self.conn.commit()
+
---
+++ test_metadata_store.py
@@ -0,0 +1,46 @@
+from catelab_store import CatelabStore
+from unittest import TestCase
+from os import unlink
+
+
+class TestMetadatStore(TestCase):
+    def setUp(self):
+        self.store = CatelabStore('.')
+
+    def test_can_write_arbitrary_config_key(self):
+        self.store.write_config_key(
+            '_UNIT_TEST_DUMMY_KEY_1', '_UNIT_TEST_DUMMY_VAL_1'
+        )
+
+    def test_can_write_and_read_arbitrary_config_key(self):
+        self.store.write_config_key(
+            '_UNIT_TEST_DUMMY_KEY_2', '_UNIT_TEST_DUMMY_VAL_2'
+        )
+
+        assert self.store.load_config_key('_UNIT_TEST_DUMMY_KEY_2') == '_UNIT_TEST_DUMMY_VAL_2'
+
+    def test_can_update_previously_written_config_key(self):
+        self.store.write_config_key(
+            '_UNIT_TEST_DUMMY_KEY_3', '_UNIT_TEST_DUMMY_VAL_3a'
+        )
+
+        assert self.store.load_config_key('_UNIT_TEST_DUMMY_KEY_3') == '_UNIT_TEST_DUMMY_VAL_3a'
+
+        self.store.write_config_key(
+            '_UNIT_TEST_DUMMY_KEY_3', '_UNIT_TEST_DUMMY_VAL_3b'
+        )
+
+        assert self.store.load_config_key('_UNIT_TEST_DUMMY_KEY_3') == '_UNIT_TEST_DUMMY_VAL_3b'
+
+    def test_can_clear_all_data(self):
+        self.store.write_config_key(
+            '_UNIT_TEST_DUMMY_KEY_4', '_UNIT_TEST_DUMMY_VAL_4'
+        )
+
+        assert self.store.load_config_key('_UNIT_TEST_DUMMY_KEY_4') == '_UNIT_TEST_DUMMY_VAL_4'
+        self.store.clear()
+        assert len(self.store.load_config_key('_UNIT_TEST_DUMMY_KEY_4')) == 0
+
+    def tearDown(self):
+        unlink(CatelabStore._METADATA_STORE_FILENAME)  # noqa
+
