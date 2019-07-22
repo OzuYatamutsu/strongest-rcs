f385e9bfa0a9451dc9713b41537445a7c9eb0c37
Sean Collins <secollins@linkedin.com>
2019-07-22 10:36:17 -0700

    Happy halloween

--- plugins/daysuntil.go
+++ plugins/daysuntil.go
@@ -9,7 +9,7 @@ import (
 )
 
 func PluginDaysUntil() {
-	targetDate := time.Date(2019, 7, 20, 0, 0, 0, 0, time.Local)
+	targetDate := time.Date(2019, 8, 31, 0, 0, 0, 0, time.Local)
 	currentDate := time.Now()
 	daysUntil := targetDate.Sub(currentDate)
 
