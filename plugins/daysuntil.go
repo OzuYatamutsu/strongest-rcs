6243072f8f4005b38aa3468f6f3e8694bc02271a
Sean Collins <seanc@seancotech.com>
2025-03-15 12:06:58 +0800

    Date until farm work

--- plugins/daysuntil.go
+++ plugins/daysuntil.go
@@ -9,7 +9,7 @@ import (
 )
 
 func PluginDaysUntil() {
-	targetDate := time.Date(2021, 01, 20, 0, 0, 0, 0, time.Local)
+	targetDate := time.Date(2025, 03, 28, 0, 0, 0, 0, time.Local)
 	currentDate := time.Now()
 	daysUntil := targetDate.Sub(currentDate)
 
