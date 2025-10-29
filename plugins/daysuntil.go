a3b56450701aa2dd5ac4a3e2a4676f0bd8f7eb63
Sean Lam <seanl@seancotech.com>
2025-10-29 13:20:57 +0800

    Update date

--- plugins/daysuntil.go
+++ plugins/daysuntil.go
@@ -1,22 +1,22 @@
 package plugins
 
 import (
-	"fmt"
-	"strconv"
-	"time"
+    "fmt"
+    "strconv"
+    "time"
 
-	"github.com/fatih/color"
+    "github.com/fatih/color"
 )
 
 func PluginDaysUntil() {
-	targetDate := time.Date(2025, 10, 30, 0, 0, 0, 0, time.Local)
-	currentDate := time.Now()
-	daysUntil := targetDate.Sub(time.Date(
+    targetDate := time.Date(2025, 11, 19, 0, 0, 0, 0, time.Local)
+    currentDate := time.Now()
+    daysUntil := targetDate.Sub(time.Date(
         currentDate.Year(), currentDate.Month(), currentDate.Day(),
         0, 0, 0, 0, currentDate.Location(),
     ))
 
-	fmt.Print(color.GreenString(" i ") + "There are ")
-	fmt.Print(color.GreenString(strconv.FormatInt(int64(daysUntil.Hours()/24), 10)))
-	fmt.Print(" days until " + color.GreenString(targetDate.Format("January 2, 2006")) + ".")
+    fmt.Print(color.GreenString(" i ") + "There are ")
+    fmt.Print(color.GreenString(strconv.FormatInt(int64(daysUntil.Hours()/24), 10)))
+    fmt.Print(" days until " + color.GreenString(targetDate.Format("January 2, 2006")) + ".")
 }
