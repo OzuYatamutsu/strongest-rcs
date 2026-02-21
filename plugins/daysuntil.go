100636e5b42c71f941cdd48a06f51882b9279521
Sean Lam <seanl@seancotech.com>
2026-02-20 20:16:43 -0800

    Update date

--- plugins/daysuntil.go
+++ plugins/daysuntil.go
@@ -9,7 +9,7 @@ import (
 )
 
 func PluginDaysUntil() {
-    targetDate := time.Date(2026, 1, 31, 0, 0, 0, 0, time.Local)
+    targetDate := time.Date(2026, 2, 17, 0, 0, 0, 0, time.Local)
     currentDate := time.Now()
     daysUntil := targetDate.Sub(time.Date(
         currentDate.Year(), currentDate.Month(), currentDate.Day(),
